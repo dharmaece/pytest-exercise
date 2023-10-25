fe54a064585c3366dfb193c2820e14ef8d25c50f
dharmaece <pdharmarajaece@gmail.com>
2023-10-25 22:00:29 +0530

    example for appending command line agrument in pytest run command

--- SamplePyTest/conftest.py
+++ SamplePyTest/conftest.py
@@ -8,8 +8,13 @@ def general_setup():
     print("\n general_setup fixture function will be applicable across all the file, since the scope is defined for session and autouse as True...")
 
 @pytest.fixture()
-def addition_setup():
+def addition_setup(calculationtype):
     print("\n going to perform addition operation for int data type variables")
+    if calculationtype == "addition":
+        print("\n it will perform addition operation")
+    else:
+        print("\n not a valid calculationtype. it support only addition")
+
 
 @pytest.fixture()
 def welcome_setup():
@@ -19,5 +24,14 @@ def welcome_setup():
 @pytest.fixture(params=["a","b"])
 def test_alphabet(request):
     print("\n fixture function call counting using alphabet variable:",request.param,"\n")
+
+def pytest_addoption(parser):
+    parser.addoption("--calculationtype")
+
+@pytest.fixture()
+def calculationtype(request):
+    return request.config.getoption("--calculationtype")
+
+
 
 
